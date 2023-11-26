fe22e94a2f57137968641f460d4ee84f0caa8726
Luka Janketić <luka.janketic@gostudion.com>
2023-11-26 22:02:24 +0100

    handle color change

---
+++ index.js
@@ -0,0 +1,14 @@
+let arrayOfClickedSquares = []
+
+function colorHandler(event) {
+    const clickedSquare = document.getElementById(event)
+    console.log(clickedSquare.style)
+    if(arrayOfClickedSquares.includes(clickedSquare)) {
+        const matchingGreenSquareIndex = arrayOfClickedSquares.indexOf(clickedSquare);
+        arrayOfClickedSquares.splice(matchingGreenSquareIndex, 1)
+        clickedSquare.style.backgroundColor = "black"
+    } else {
+        arrayOfClickedSquares.push(clickedSquare)
+        clickedSquare.style.backgroundColor = "green"
+    }
+} 
